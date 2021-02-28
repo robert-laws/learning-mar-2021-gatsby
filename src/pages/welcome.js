f12ba2b939f75e258e34ea43b5eb26e442b3da11
Robert Laws <roblaws@me.com>
2021-02-28 16:11:44 +0300

    add images

---
+++ src/pages/welcome.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Welcome = () => {
+  return (
+    <Layout>
+      <h1>Welcome!</h1>
+      <figure>
+        <img src="/staff/rdl.jpg" alt="library staff" />
+      </figure>
+      {`hello...`}
+    </Layout>
+  )
+}
+
+export default Welcome
